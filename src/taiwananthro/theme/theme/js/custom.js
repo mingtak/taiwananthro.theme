28247939a3bfe2c91527e01ba8967e445b565fca
Henry Zhuang <henry@mingtak.com.tw>
2018-05-17 13:30:41 +0800

    update 0517-2

--- src/taiwananthro/theme/theme/js/custom.js
+++ src/taiwananthro/theme/theme/js/custom.js
@@ -2,5 +2,15 @@ $(document).ready(function(){
     $('.article-item .show-abs').click(function(){
         $(this).prev('.article-abs').slideToggle()
     })
+/*
+    $('#sc_li').click(function(){
+        $(this).attr('class', 'active')
+        if( $(this).attr('id') == 'tc_li'){
+            $('#sc_li').attr('class', '')
+        }else{
+            $('#tc_li').attr('class', '')
+        }
+    })
+*/
 })
 
